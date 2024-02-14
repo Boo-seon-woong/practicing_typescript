3c855adc9e8265b01d12ce5e325dbee652f799cf
Boo-seon-woong <boosunung@gmail.com>
2024-02-14 20:24:59 +0900

    난 개발환경 구축이 싫어

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("hello javascript and typescript!");
+console.log("and also we got a tsconfig too lol");
+console.log("and also npm init -y, nodemon, currently also hello! from now on, build and run will be simultaneously processed with only comment 'npm start' becuase of tsconfig")
