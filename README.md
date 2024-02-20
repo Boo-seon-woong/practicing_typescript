a1a83514ebade134855ae8415e3b8a798a6dc044
Boo-seon-woong <boosunung@gmail.com>
2024-02-20 21:13:19 +0900

    today's last commit....maybe

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 <br/>일단은 유튜브에 있는 나무소리 typescript 재생목록 기반으로 공부하면서 todo list 만들어보면서 기초 익히는게 목표
 <h1>공부 시작할 때 제일 하기 싫은거 : 개발환경 구축</h1>
 <p>
-    뭔 typescript여 그냥 ES6부터 공부하고 와라 선웅아<br>src 디렉토리에서 .ts 파일 지우거나 이름 변경했을때 tsconfig.json이 발작하면서 File '파일경로' not found 라고 개거품무는데 그럴때는 ctrl+shift+p 한 다음 Typescript: Restart TS Server 하면 해결되드라. 설마 readme에 적어놨는데 또 헷갈리지는 않겠지......<br>
+    뭔 typescript여 그냥 ES6부터 공부하고 와라 선웅아<br>src 디렉토리에서 .ts 파일 지우거나 이름 변경했을때 tsconfig.json이 발작하면서 File '파일경로' not found 라고 개거품무는데 그럴때는 ctrl+shift+p 한 다음 Typescript: Restart TS Server 하면 해결되드라. 설마 readme에 적어놨는데 또 헷갈리지는 않겠지......<br>  ㄴ라고 생각한지 10분도 안돼서 걍 typescript 공부 강행. 필요한 개념은 그때그때 갖다쓰면 어케 되지 않을까.....?
 </p>
--- build/index.js
+++ build/index.js
@@ -1,4 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const TodoItem_1 = __importDefault(require("./TodoItem"));
 console.log("hello javascript and typescript!");
 console.log("and also we got a tsconfig too lol");
 console.log("\n\n\nand also npm init -y, nodemon, currently also hello! from now on, build and run will be simultaneously processed with only comment 'npm start' becuase of tsconfig");
+const data = [
+    { id: 1, task: 'a', complete: false },
+    { id: 2, task: 'b', complete: true }
+];
+console.log('my todo list\n');
+for (let i = 0; i < data.length; i++) {
+    let todoitem = new TodoItem_1.default(data[i].id, data[i].task, data[i].complete);
+    todoitem.printDetails();
+}
