017985596ed963d649ad8c6eec3911d1fc1c55ad
Boo-seon-woong <boosunung@gmail.com>
2024-02-20 20:37:25 +0900

    I have this

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import TodoItem from "./TodoItem";
+
 console.log("hello javascript and typescript!");
 console.log("and also we got a tsconfig too lol");
 console.log("\n\n\nand also npm init -y, nodemon, currently also hello! from now on, build and run will be simultaneously processed with only comment 'npm start' becuase of tsconfig")
+
+const data = [
+    {id:1, task:'a',complete:false},
+    {id:2, task:'b',complete:true}
+]
+
+console.log('my todo list\n');
+
+for(let i=0;i<data.length;i++){
+    let todoitem=new TodoItem(data[i].id,data[i].task,data[i].complete);
+    todoitem.printDetails();
+}
