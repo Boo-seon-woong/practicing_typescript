43fc3aa8bd65f9c12ae8dd07041ae46c30e4dc9f
Boo-seon-woong <boosunung@gmail.com>
2024-02-21 19:49:58 +0900

    git add created

---
+++ src/data.ts
@@ -0,0 +1,4 @@
+export const data = [
+    {id:1, task:'a',complete:false},
+    {id:2, task:'b',complete:true}
+]
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import TodoItem from "./TodoItem";
-
+import { data } from "./data";
 console.log("hello javascript and typescript!");
 console.log("and also we got a tsconfig too lol");
 console.log("\n\n\nand also npm init -y, nodemon, currently also hello! from now on, build and run will be simultaneously processed with only comment 'npm start' becuase of tsconfig")
 
-const data = [
-    {id:1, task:'a',complete:false},
-    {id:2, task:'b',complete:true}
-]
+
 
 console.log('my todo list\n');
 
